ead173be64177f93164737347c6207c77b3a7ded
Juan Camilo Gutiérrez Martínez <97555202+Camilog2004@users.noreply.github.com>
2023-02-25 23:44:45 -0500

    Add files via upload

---
+++ televisores/marca.py
@@ -0,0 +1,17 @@
+class Marca:
+	#Constructor
+	def __init__(self,nombre):
+		self._nombre=nombre
+
+	#Metodos get y set
+
+	def getNombre(self):
+		return self._nombre
+
+	def setNombre(self,nombre):
+		self._nombre=nombre
+
+	
+
+
+
